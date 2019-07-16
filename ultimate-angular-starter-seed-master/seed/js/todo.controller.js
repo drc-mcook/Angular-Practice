012aa53ecc5d732f240b0acb634fe86ce8164ca5
Cook <MCook@datarecognitioncorp.com>
2019-07-16 14:11:28 -0500

    Adding full crud to todo list

--- ultimate-angular-starter-seed-master/seed/js/todo.controller.js
+++ ultimate-angular-starter-seed-master/seed/js/todo.controller.js
@@ -1,27 +1,61 @@
-function TodoController() {
-    this.newTodo = '';
-    this.list = [{
-        title: 'First todo item',
-        completed: false
-    }, {
-        title: 'Second todo item',
-        completed: false
-    }, {
-        title: 'Third todo item',
-        completed: true
-    }];
-    this.addTodo = function () {
-        this.list.unshift({
-            title: this.newTodo,
-            completed: false
+function TodoController(TodoService) {
+    var ctrl = this;
+    ctrl.newTodo = '';
+    ctrl.list = [];
+    function getTodos() {
+      TodoService
+        .retrieve()
+        .then(function (response) {
+          ctrl.list = response;
         });
-        this.newTodo = '';
     }
-    this.removeTodo = function (item, index) {
-        this.list.splice(index, 1);
+    ctrl.addTodo = function () {
+      if (!ctrl.newTodo) {
+        return;
+      }
+      TodoService
+        .create({
+          title: ctrl.newTodo,
+          completed: false
+        })
+        .then(function (response) {
+          ctrl.list.unshift(response);
+          ctrl.newTodo = '';
+        });
     };
-}
-
-angular
+    ctrl.removeTodo = function (item, index) {
+      TodoService
+        .remove(item)
+        .then(function (response) {
+          ctrl.list.splice(index, 1);
+        });
+    };
+    ctrl.updateTodo = function (item, index) {
+      if (!item.title) {
+        ctrl.removeTodo(item, index);
+        return;
+      }
+      TodoService
+        .update(item);
+    };
+    ctrl.getRemaining = function () {
+      return ctrl.list.filter(function (item) {
+        return !item.completed;
+      });
+    };
+    ctrl.toggleState = function (item) {
+      TodoService
+        .update(item)
+        .then(function () {
+  
+        }, function () {
+          item.completed = !item.completed;
+        });
+    };
+    getTodos();
+  }
+  
+  angular
     .module('app')
     .controller('TodoController', TodoController);
+  
---
+++ ultimate-angular-starter-src-master/README.md
@@ -0,0 +1,20 @@
+<a href="https://ultimateangular.com" target="_blank"><img src="https://ultimateangular.com/assets/img/banners/ua-github.svg"></a>
+
+# Ultimate Angular: Fundamentals Source Code
+
+> This repo serves as the source files for the Ultimate Angular 1.x Fundamentals course. Come and [learn Angular](https://ultimateangular.com) 1.5 architecture, latest features, components and more!
+
+# Starter Sourcecode
+
+The Starter Course fundamentals source code for Ultimate Angular 1.x [Fundamentals course](https://ultimateangular.com).
+
+### Running these projects
+
+Some of these may require a local server, however most do not. All you need to do is run a local server, if you're on OSX you can run a python command from the command line:
+
+```
+cd <ultimate-angular-starter-seed>
+python -m SimpleHTTPServer
+```
+
+If you're on Windows, you can also download and run python, or if it's easier you can setup a local server by using your favourite IDE.
